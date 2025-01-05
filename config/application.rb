8fa3d34819639f61aa4f5a33679ff98a8b57f54e
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-01-05 17:51:31 +0900

    ✨ feat(application): ジェネレータのヘルパー生成を無効化
    🏠 feat(home_controller): HomeControllerを追加し、indexアクションを定義
    
    🖼️ feat(home/index): ホームページのビューを作成
    
    🔧 chore(routes): ルートパスをhome#indexに設定

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module LifeQuest
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.generators do |g|
+      g.helper false
+    end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
