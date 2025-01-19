e0d7672d3ad294b16611c6aee82922ec16acb045
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-01-19 12:25:20 +0900

    credential情報を含まないdocker imageを作るために

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,5 @@
+return if ENV["SECRET_KEY_BASE_DUMMY"].present?
+
 OpenAI.configure do |config|
   config.access_token = Rails.application.credentials.openai.access_token
   config.log_errors = Rails.env.local?
