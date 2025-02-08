05d1ad2637603f801fd7bb28c1b106bf005e233e
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-02-09 04:01:02 +0900

    service workerを登録できるように

---
+++ app/javascript/register-service-worker.js
@@ -0,0 +1,12 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js").then(
+    (registration) => {
+      console.log("Service worker registration succeeded:", registration);
+    },
+    (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    }
+  );
+} else {
+  console.error("Service workers are not supported.");
+}
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resource :push_subscriptions, only: %i[ create destroy ]
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
